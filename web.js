2b4ddfe7ae828db080585f78db7070913d88616c
Francois d'Erneville <derneville@gmail.com>
2013-07-18 20:49:41 +0000

    Added web.js

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var fs = require('fs');
+    var htmltext = fs.readFileSync('index.html');
+    response.send(htmltext.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
